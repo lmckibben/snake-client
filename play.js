7b0e4101823ada4cd64a825aa22f20f2bc37369b
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-25 19:59:40 +0000

    refactored play.js removing setupInput.js, created input.js and added SetupInput to it, exported from input.js and imported to play.js

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const net = require("net");
 const connect = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function (handleUserInput) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
-setupInput(handleUserInput);
+setupInput();
 
